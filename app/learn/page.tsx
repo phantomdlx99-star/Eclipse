1c09a938e47cd706764f0ee2d09e40a0fe21342c
Phoenix <phoenixsigil68@gmail.com>
2026-02-01 16:40:52 +0530

    Redirection logic for learn page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { GSEB_CURRICULUM } from "@/lib/constant";
 import ClassCard from "@/components/ClassCard";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
+
   return (
     <main className="w-full h-auto px-10 py-5 grid grid-cols-1 sm:grid-cols-2 gap-5">
       {GSEB_CURRICULUM.map((item: any) => (
