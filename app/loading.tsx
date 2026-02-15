6662cfb972d6107c51c9951531b4f4348183475f
Phoenix <phoenixsigil68@gmail.com>
2026-02-15 13:03:25 +0530

    Changed Loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,10 @@
 // app/products/loading.tsx
-
+import { HashLoader } from "react-spinners";
 export default function Loading() {
   // You can add any UI inside, including a Skeleton.
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-      <p className="ml-4 text-lg">Loading...</p>
+      <HashLoader color="#1e96dc" />
     </div>
   );
 }
